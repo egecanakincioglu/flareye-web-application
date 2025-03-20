17cb197b4a939b600e3d9f9051cffea250fd619c
Egecan Akıncıoğlu <egecanakincioglu@outlook.com>
2025-03-20 20:24:36 +0100

    Deleted the faulty node modules file, installed a new backend microservice structure

---
+++ backend/services/api-key-service/src/main/java/com/flareye/apikeyservice/ApiKeyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flareye.apikeyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKeyServiceApplication.class, args);
+    }
+
+}
---
+++ backend/services/api-key-service/src/test/java/com/flareye/apikeyservice/ApiKeyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flareye.apikeyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
