3cdd3098e2aa1c5e0416079a5388a1d6a2664661
Egecan Akıncıoğlu <egecanakincioglu@outlook.com>
2025-03-21 04:32:59 +0100

    Remove hashing method

--- backend/services/authentication-service/src/main/java/com/flareye/authenticationservice/services/UserService.java
+++ backend/services/authentication-service/src/main/java/com/flareye/authenticationservice/services/UserService.java
@@ -3,7 +3,6 @@ package com.flareye.authenticationservice.services;
 import com.flareye.authenticationservice.models.User;
 import com.flareye.authenticationservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,15 +12,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
     public User saveUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword())); // BCRYPT ŞIFRELEME METHODU
+        // 🔴 Hata: Burada tekrar şifre hashleme YAPILMAMALI!
+        System.out.println("Kayıt edilen şifre (hashlenmiş olarak alındı): " + user.getPassword());
+
         return userRepository.save(user);
     }
 }
